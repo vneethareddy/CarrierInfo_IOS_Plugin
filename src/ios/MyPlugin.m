fd7770a5f552259610c663dd9d8c4ab86faab8db
vneethareddy <vneethareddy@gmail.com>
2014-11-11 14:40:22 +0530

    Update MyPlugin.m

--- src/ios/MyPlugin.m
+++ src/ios/MyPlugin.m
@@ -8,26 +8,36 @@
 - (void)getCarrierInfo:(CDVInvokedUrlCommand*)command
 {
     
-    NSString *currentDate =[self getCurrentDate];
+ NSDictionary *currentDate =[self getCurrentDate];
     
     
     
-  CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:currentDate];
+  CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:currentDate];
   [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
-- (NSString *) getCurrentDate{
+- (NSDictionary *) getCurrentDate{
     
     
     
 //    CTTelephonyNetworkInfo *netinfo = [[CTTelephonyNetworkInfo alloc] init];
 //    CTCarrier *carrier = [netinfo subscriberCellularProvider];
 //    NSLog(@"Carrier Name: %@", [carrier carrierName]);
-//    NSString *dateString = [carrier carrierName];
-   NSString* uniqueIdentifier = [[[UIDevice currentDevice] identifierForVendor] UUIDString]; // IOS 6+
-    NSLog(@"UDID:: %@", uniqueIdentifier);
-      return uniqueIdentifier;
- 
+      NSDate *currDate = [NSDate date];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
+    [dateFormatter setDateFormat:@"dd.MM.YY HH:mm:ss"];
+    NSString *dateString = [dateFormatter stringFromDate:currDate];
     
+    NSString* uniqueIdentifier = [[[UIDevice currentDevice] identifierForVendor] UUIDString]; // IOS 6+
+    NSLog(@"UDID:: %@", uniqueIdentifier);
+     NSMutableDictionary* devProps = [NSMutableDictionary dictionaryWithCapacity:3];
+    
+    [devProps setObject:uniqueIdentifier forKey:@"model"];
+    [devProps setObject:@"iOS" forKey:@"platform"];
+    [devProps setObject:dateString forKey:@"version"];
+    NSLog(@"UDID:: %@", devProps);
+    
+    NSDictionary* devReturn = [NSDictionary dictionaryWithDictionary:devProps];
+    return devReturn;
 }
 
 @end
